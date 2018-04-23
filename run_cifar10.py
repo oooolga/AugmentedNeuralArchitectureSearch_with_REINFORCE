826d74aec6098923d3f77eeef2f6dd77fccefdd7
oooolga <olga.xu823@gmail.com>
2018-04-23 04:44:01 -0400

    add different cuda devices

--- run_cifar10.py
+++ run_cifar10.py
@@ -39,13 +39,13 @@ def parse():
 
 	parser.add_argument('--batch-size', default=128, type=int,
 						help='Mini-batch size')
-	parser.add_argument('--max-layers', default=8, type=int,
+	parser.add_argument('--max-layers', default=10, type=int,
 						help='Max number of layers')
 	parser.add_argument('--alpha', default=0.7, type=float, help='Alpha')
 	parser.add_argument('--gamma', default=1.0, type=float, help='Discounting factor (gamma)')
 	parser.add_argument('-lr', '--learning_rate', default=1e-4, type=float,
 						help='Learning rate')
-	parser.add_argument('--num-episodes', default=200, type=int, help='Number of episodes')
+	parser.add_argument('--num-episodes', default=500, type=int, help='Number of episodes')
 	parser.add_argument('--model-dir', default='./saved_model', type=str, help='Directory for saved models')
 	parser.add_argument('--model-name', default='cifar10_', type=str, help='Model name')
 	parser.add_argument('--check-memory', action='store_true', help='Flag for check memory leak')
@@ -61,83 +61,84 @@ def cifar_env(layer_list, train_loader, valid_loader, learning_rate,
 	net, optimizer = None, None
 
 	try:
-		net = Network(layer_list=layer_list, w_in=32, h_in=32, c_in=3, out_dim=10)
-		if use_cuda:
-			net.cuda()
+		with torch.cuda.device(available_devices[-1]):
+			net = Network(layer_list=layer_list, w_in=32, h_in=32, c_in=3, out_dim=10)
+			if use_cuda:
+				net.cuda()
 
-		optimizer = optim.Adam(params=net.parameters(), lr=learning_rate)
+			optimizer = optim.Adam(params=net.parameters(), lr=learning_rate)
 
-		def train(net, optimizer, train_loader):
-			net.train()
+			def train(net, optimizer, train_loader):
+				net.train()
 
-			for epoch_i in range(10):
-				print('|\t\t\tTrain epoch {}:'.format(epoch_i+1))
-				total_loss = 0
-				total_batch = 0
-				for batch_idx, (data, target) in enumerate(train_loader):
-					if use_cuda:
-						data, target = data.cuda(), target.cuda()
+				for epoch_i in range(10):
+					print('|\t\t\tTrain epoch {}:'.format(epoch_i+1))
+					total_loss = 0
+					total_batch = 0
+					for batch_idx, (data, target) in enumerate(train_loader):
+						if use_cuda:
+							data, target = data.cuda(), target.cuda()
 
-					data, target = Variable(data, requires_grad=False), \
-							   Variable(target, requires_grad=False)
-					optimizer.zero_grad()
-					pred = net(data)
-					loss = net.loss(pred, target)
-					total_loss += loss
-					loss.backward()
-					optimizer.step()
-					total_batch += 1
+						data, target = Variable(data, requires_grad=False), \
+								   Variable(target, requires_grad=False)
+						optimizer.zero_grad()
+						pred = net(data)
+						loss = net.loss(pred, target)
+						total_loss += loss
+						loss.backward()
+						optimizer.step()
+						total_batch += 1
 
-				print('|\t\t\t\tAverage loss={:.4f}'.format(total_loss/float(total_batch)))
-				del data, target, pred, loss, total_loss
+					print('|\t\t\t\tAverage loss={:.4f}'.format(total_loss/float(total_batch)))
+					del data, target, pred, loss, total_loss
 
 
-		def eval(net, data_loader):
-			net.eval()
+			def eval(net, data_loader):
+				net.eval()
 
-			total_loss, correct = 0, 0
-			total_data, total_batch = 0, 0
+				total_loss, correct = 0, 0
+				total_data, total_batch = 0, 0
 
-			for batch_idx, (data, target) in enumerate(data_loader):
-				with torch.no_grad(): # prevent memory leak
-					if use_cuda:
-						data, target = data.cuda(), target.cuda()
+				for batch_idx, (data, target) in enumerate(data_loader):
+					with torch.no_grad(): # prevent memory leak
+						if use_cuda:
+							data, target = data.cuda(), target.cuda()
 
-					data, target = Variable(data, requires_grad=False), \
-							   Variable(target, requires_grad=False)
+						data, target = Variable(data, requires_grad=False), \
+								   Variable(target, requires_grad=False)
 
-					pred = net(data)
-					_, predicted = torch.max(pred.data, 1)
-					correct += (predicted == target.data).sum()
-					total_data += len(data)
+						pred = net(data)
+						_, predicted = torch.max(pred.data, 1)
+						correct += (predicted == target.data).sum()
+						total_data += len(data)
 
-				del data, target, pred, predicted
+					del data, target, pred, predicted
 
-			accuracy = correct.cpu().numpy() / float(total_data)
-			del correct
+				accuracy = correct.cpu().numpy() / float(total_data)
+				del correct
 
-			return accuracy
+				return accuracy
 
-		train(net, optimizer, train_loader)
-		valid_accuracy = eval(net, valid_loader)
+			train(net, optimizer, train_loader)
+			valid_accuracy = eval(net, valid_loader)
 
-		test_accuracy = None
-		if test_loader:
-			test_accuracy = eval(net, optimizer, test_loader)
+			test_accuracy = None
+			if test_loader:
+				test_accuracy = eval(net, optimizer, test_loader)
 
-		global best_accuracy
-		if valid_accuracy > best_accuracy:
-			best_accuracy = valid_accuracy
-			save_checkpoint({'layer_list': layer_list, 'state_dict': net.state_dict(),
-							  'optimizer': optimizer.state_dict()},
-							  os.path.join(model_dir, model_name+'reinforce_best.pt'))
-			del net
-			del optimizer
-			return valid_accuracy, test_accuracy, None
-		else:
-			del net
-			del optimizer
-			return valid_accuracy, test_accuracy, None
+			global best_accuracy
+			if valid_accuracy > best_accuracy:
+				best_accuracy = valid_accuracy
+				save_checkpoint({'layer_list': layer_list, 'state_dict': net.state_dict(),
+								  'optimizer': optimizer.state_dict()},
+								  os.path.join(model_dir, model_name+'reinforce_best.pt'))
+				del net
+				del optimizer
+				return valid_accuracy, test_accuracy, None
+			else:
+				del net
+				del optimizer
+				return valid_accuracy, test_accuracy, None
 
 	except Exception as e:
 		del net
@@ -159,46 +160,93 @@ if __name__ == '__main__':
 															  test_batch_size=args.batch_size,
 															  alpha=args.alpha)
 
-	start_epi = 1
-	if args.load_model:
-		replay_tree, REINFORCE_policy_net, optimizer, epi_i, count_accuracy, avg_accuracy, total_architectures = \
-			load_reinforce_model(args.load_model)
+	with torch.cuda.device(0):
 
-	else:
-		REINFORCE_policy_net = Policy(NUM_LAYERS_TYPE, 32, args.gamma)
-		if use_cuda:
-			REINFORCE_policy_net.cuda()
-		optimizer = optim.Adam(params=REINFORCE_policy_net.parameters(), lr=args.learning_rate)
+		start_epi = 1
+		if args.load_model:
+			replay_tree, REINFORCE_policy_net, optimizer, epi_i, count_accuracy, avg_accuracy, total_architectures = \
+				load_reinforce_model(args.load_model)
 
-	all_accuracy = []
-	global model_dir, model_name
-	model_dir = args.model_dir
-	model_name = args.model_name
+		else:
+			REINFORCE_policy_net = Policy(NUM_LAYERS_TYPE, 32, args.gamma)
+			if use_cuda:
+				REINFORCE_policy_net.cuda()
+			optimizer = optim.Adam(params=REINFORCE_policy_net.parameters(), lr=args.learning_rate)
 
-	for epi_i in range(start_epi, args.num_episodes+start_epi):
-		print('|\tEpisode #{}:'.format(epi_i))
+		all_accuracy = []
+		global model_dir, model_name
+		model_dir = args.model_dir
+		model_name = args.model_name
 
-		if args.check_memory:
-			pid = os.getpid()
-			prev_mem = 0
+		for epi_i in range(start_epi, args.num_episodes+start_epi):
+			print('|\tEpisode #{}:'.format(epi_i))
 
-		new_action_state = Variable(torch.zeros(1, NUM_LAYERS_TYPE), requires_grad=False)
-		if use_cuda:
-			new_action_state = new_action_state.cuda()
+			if args.check_memory:
+				pid = os.getpid()
+				prev_mem = 0
 
-		layer_list = []
-		for layer_i in range(args.max_layers-1):
-			total_architectures += 1
-			print('|\t\tArchitecture #{} ({}):'.format(total_architectures, layer_i+1))
-			new_action_state, new_action = REINFORCE_policy_net.select_action(new_action_state)
+			new_action_state = Variable(torch.zeros(1, NUM_LAYERS_TYPE), requires_grad=False)
+			if use_cuda:
+				new_action_state = new_action_state.cuda()
 
-			if new_action == NUM_LAYERS_TYPE-1:
-				if layer_i == 0:
-					displayModelSetting([])
-					reward, _, failed = cifar_env([], train_loader, valid_loader,
+			layer_list = []
+			for layer_i in range(args.max_layers-1):
+				total_architectures += 1
+				print('|\t\tArchitecture #{} ({}; Episode {}):'.format(total_architectures, layer_i+1, epi_i))
+				new_action_state, new_action = REINFORCE_policy_net.select_action(new_action_state)
+
+				if new_action == NUM_LAYERS_TYPE-1:
+					if layer_i == 0:
+						displayModelSetting([])
+						reward, _, failed = cifar_env([], train_loader, valid_loader,
+															args.learning_rate)
+						print('|\t\t\tValidation accuracy={:.4f} [highest: {:.4f}, avg: {:.4f}]'.format(reward, best_accuracy, avg_accuracy))
+						REINFORCE_policy_net.rewards.append(reward)
+						if count_accuracy == 0:
+							avg_accuracy = reward 
+							count_accuracy += 1
+						else:
+							count_accuracy += 1
+							avg_accuracy = 1/float(count_accuracy)*reward +\
+										   float(count_accuracy-1)/float(count_accuracy)*avg_accuracy
+					break
+
+				layer_list.append(LAYERS_TYPE[new_action])
+				displayModelSetting(layer_list)
+
+				## Expeerience Replay
+				if curr_node.check_child_exist(new_action):
+					tmp = random_sample()
+					curr_node = curr_node.get_child(new_action)
+					reward0 = curr_node.get_value()
+			
+					if reward0 == 0.0 or tmp > 0.1:
+						# REPLAY!
+						print('|\t\t\tExperience Replay')
+						reward = reward0
+						failed = False
+						if reward == 0:
+							failed = True
+					else:					
+						reward1, _, failed =  cifar_env(layer_list, train_loader, valid_loader,
 														args.learning_rate)
-					print('|\t\t\tValidation accuracy={:.4f}'.format(reward))
-					REINFORCE_policy_net.rewards.append(reward)
+
+						if count_accuracy == 0:
+							avg_accuracy = reward1
+							count_accuracy += 1
+						else:
+							count_accuracy += 1
+							avg_accuracy = 1/float(count_accuracy)*reward1 +\
+										   float(count_accuracy-1)/float(count_accuracy)*avg_accuracy
+
+						curr_node.add_count()
+						reward = 1/float(curr_node.get_count())*reward1 + \
+							float(curr_node.get_count()-1)/float(curr_node.get_count())*reward0
+
+						curr_node.update_value(reward)
+				else:
+					reward, _, failed =  cifar_env(layer_list, train_loader, valid_loader,
+														 args.learning_rate)
 					if count_accuracy == 0:
 						avg_accuracy = reward 
 						count_accuracy += 1
@@ -206,85 +254,40 @@ if __name__ == '__main__':
 						count_accuracy += 1
 						avg_accuracy = 1/float(count_accuracy)*reward +\
 									   float(count_accuracy-1)/float(count_accuracy)*avg_accuracy
-				break
 
-			layer_list.append(LAYERS_TYPE[new_action])
-			displayModelSetting(layer_list)
+					new_experience_node = TreeNode(new_action, reward)
+					curr_node.add_child(new_experience_node)
+					curr_node = new_experience_node
 
-			## Expeerience Replay
-			if curr_node.check_child_exist(new_action):
-				tmp = random_sample()
-				curr_node = curr_node.get_child(new_action)
-				reward0 = curr_node.get_value()
-		
-				if reward0 == 0.0 or tmp > 0.1:
-					# REPLAY!
-					print('|\t\t\tExperience Replay')
-					reward = reward0
-					failed = False
-					if reward == 0:
-						failed = True
-				else:					
-					reward1, _, failed =  cifar_env(layer_list, train_loader, valid_loader,
-													args.learning_rate)
+				print('|\t\t\tValidation accuracy={:.4f} [highest: {:.4f}, avg: {:.4f}]'.format(reward, best_accuracy, avg_accuracy))
+				REINFORCE_policy_net.rewards.append(reward)
 
-					if count_accuracy == 0:
-						avg_accuracy = reward1
-						count_accuracy += 1
-					else:
-						count_accuracy += 1
-						avg_accuracy = 1/float(count_accuracy)*reward1 +\
-									   float(count_accuracy-1)/float(count_accuracy)*avg_accuracy
+				if args.check_memory:
+					cur_mem = (int(open('/proc/%s/statm'%pid, 'r').read().split()[1])+0.0)/256
+					add_mem = cur_mem - prev_mem
+					prev_mem = cur_mem
+					print("|\t\tadded mem: %sM"%(add_mem))
 
-					curr_node.add_count()
-					reward = 1/float(curr_node.get_count())*reward1 + \
-						float(curr_node.get_count()-1)/float(curr_node.get_count())*reward0
+				if failed:
+					break
 
-					curr_node.update_value(reward)
-			else:
-				reward, _, failed =  cifar_env(layer_list, train_loader, valid_loader,
-													 args.learning_rate)
-				if count_accuracy == 0:
-					avg_accuracy = reward 
-					count_accuracy += 1
-				else:
-					count_accuracy += 1
-					avg_accuracy = 1/float(count_accuracy)*reward +\
-								   float(count_accuracy-1)/float(count_accuracy)*avg_accuracy
+			optimizer.zero_grad()
+			model_loss = REINFORCE_policy_net.finish_episode(avg_accuracy)
+			model_loss.backward()
+			optimizer.step()
 
-				new_experience_node = TreeNode(new_action, reward)
-				curr_node.add_child(new_experience_node)
-				curr_node = new_experience_node
+			del model_loss
 
-			print('|\t\t\tValidation accuracy={:.4f}'.format(reward))
-			REINFORCE_policy_net.rewards.append(reward)
+			curr_node = replay_tree
 
-			if args.check_memory:
-				cur_mem = (int(open('/proc/%s/statm'%pid, 'r').read().split()[1])+0.0)/256
-				add_mem = cur_mem - prev_mem
-				prev_mem = cur_mem
-				print("|\t\tadded mem: %sM"%(add_mem))
-
-			if failed:
-				break
-
-		optimizer.zero_grad()
-		model_loss = REINFORCE_policy_net.finish_episode(avg_accuracy)
-		model_loss.backward()
-		optimizer.step()
-
-		del model_loss
-
-		curr_node = replay_tree
-
-		if (epi_i+1) % args.save_freq == 0:
-			save_checkpoint({'args': args,
-							 'state_dict': REINFORCE_policy_net.state_dict(),
-							 'optimizer': optimizer.state_dict(),
-							 'replay_tree': replay_tree,
-							 'epi_i': epi_i,
-							 'count_accuracy': count_accuracy,
-							 'avg_accuracy': avg_accuracy,
-							 'total_architectures': total_architectures},
-							  os.path.join(model_dir, model_name+'reinforce_{}.pt'.format(epi_i+1)))
+			if (epi_i+1) % args.save_freq == 0:
+				save_checkpoint({'args': args,
+								 'state_dict': REINFORCE_policy_net.state_dict(),
+								 'optimizer': optimizer.state_dict(),
+								 'replay_tree': replay_tree,
+								 'epi_i': epi_i,
+								 'count_accuracy': count_accuracy,
+								 'avg_accuracy': avg_accuracy,
+								 'total_architectures': total_architectures},
+								  os.path.join(model_dir, model_name+'reinforce_{}.pt'.format(epi_i+1)))
 
